45d70ed65c8dc97c8a6f99594d45e4e46e645e30
Matthew Wood (0woodm62) <0woodm62@solent.ac.uk>
2017-04-10 15:21:28 +0100

    Main Page Setup -V1

---
+++ docs/dist/index.php
@@ -0,0 +1,206 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="/css/bootstrap.min.css">
+  </head>
+  <body>
+      <div class="container-fluid">
+          <nav class="navbar navbar-toggleable-md navbar-light bg-faded" id="Navbar">
+            <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <a class="navbar-brand" href="#">IMS</a>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                  <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link disabled" href="#">Disabled</a>
+                </li>
+              </ul>
+              <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="text" placeholder="Search">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+              </form>
+            </div>
+          </nav>
+          <div class="row">
+              <aside id="SideNavigation">
+                  <ul>
+                    <li><a href="#">SideBar Link</a></li>
+                    <li><a href="#">SideBar Link</a></li>
+                    <li><a href="#">SideBar Link</a></li>
+                    <li><a href="#">SideBar Link</a></li>
+                    <li><a href="#">SideBar Link</a></li>
+                    <li><a href="#">SideBar Link</a></li>
+                    <li><a href="#">SideBar Link</a></li>
+                  </ul>
+              </aside>
+              <section id="MainContentArea">
+                 <article>
+                    <h1>Alerts</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                 </article>
+                 <article>
+                     <header>
+                         <h1>Vew Assets</h1>
+                     </header>
+                    <table class="AssetsTable table table-hover">
+                    <thead class="thead">
+                        <tr>
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th>Type</th>
+                            <th>Added</th>
+                            <th>Location</th>
+                            <th>Registered User</th>
+                            <th>View Details</th>
+                        </tr>
+                    </thead>
+                        <tr>
+                            <td>1</td>
+                            <td>PC001</td>
+                            <td>Computer</td>
+                            <td>20/02/2013</td>
+                            <td>RM602</td>
+                            <td>James Doe</td>
+                            <td><button type="button" name="button" class="btn btn-primary">More...</button></td>
+                        </tr>
+                        <tr>
+                            <td>2</td>
+                            <td>PC001</td>
+                            <td>Computer</td>
+                            <td>20/02/2013</td>
+                            <td>RM602</td>
+                            <td>James Doe</td>
+                            <td><button type="button" name="button" class="btn btn-primary">More...</button></td>
+                        </tr>
+                        <tr>
+                            <td>3</td>
+                            <td>PC001</td>
+                            <td>Computer</td>
+                            <td>20/02/2013</td>
+                            <td>RM602</td>
+                            <td>James Doe</td>
+                            <td><button type="button" name="button" class="btn btn-primary">More...</button></td>
+                        </tr>
+                        <tr>
+                            <td>4</td>
+                            <td>PC001</td>
+                            <td>Computer</td>
+                            <td>20/02/2013</td>
+                            <td>RM602</td>
+                            <td>James Doe</td>
+                            <td><button type="button" name="button" class="btn btn-primary">More...</button></td>
+                        </tr>
+                        <tr>
+                            <td>5</td>
+                            <td>PC001</td>
+                            <td>Computer</td>
+                            <td>20/02/2013</td>
+                            <td>RM602</td>
+                            <td>James Doe</td>
+                            <td><button type="button" name="button" class="btn btn-primary">More...</button></td>
+                        </tr>
+                        <tr>
+                            <td>6</td>
+                            <td>PC001</td>
+                            <td>Computer</td>
+                            <td>20/02/2013</td>
+                            <td>RM602</td>
+                            <td>James Doe</td>
+                            <td><button type="button" name="button" class="btn btn-primary">More...</button></td>
+                        </tr>
+                        <tr>
+                            <td>7</td>
+                            <td>PC001</td>
+                            <td>Computer</td>
+                            <td>20/02/2013</td>
+                            <td>RM602</td>
+                            <td>James Doe</td>
+                            <td><button type="button" name="button" class="btn btn-primary">More...</button></td>
+                        </tr>
+                        <tr>
+                            <td>8</td>
+                            <td>PC001</td>
+                            <td>Computer</td>
+                            <td>20/02/2013</td>
+                            <td>RM602</td>
+                            <td>James Doe</td>
+                            <td><button type="button" name="button" class="btn btn-primary">More...</button></td>
+                        </tr>
+                        <tr>
+                            <td>9</td>
+                            <td>PC001</td>
+                            <td>Computer</td>
+                            <td>20/02/2013</td>
+                            <td>RM602</td>
+                            <td>James Doe</td>
+                            <td><button type="button" name="button" class="btn btn-primary">More...</button></td>
+                        </tr>
+                        <tr>
+                            <td>10</td>
+                            <td>PC001</td>
+                            <td>Computer</td>
+                            <td>20/02/2013</td>
+                            <td>RM602</td>
+                            <td>James Doe</td>
+                            <td><button type="button" name="button" class="btn btn-primary">More...</button></td>
+                        </tr>
+                        <tr>
+                            <td>11</td>
+                            <td>PC001</td>
+                            <td>Computer</td>
+                            <td>20/02/2013</td>
+                            <td>RM602</td>
+                            <td>James Doe</td>
+                            <td><button type="button" name="button" class="btn btn-primary">More...</button></td>
+                        </tr>
+                        <tr>
+                            <td>12</td>
+                            <td>PC001</td>
+                            <td>Computer</td>
+                            <td>20/02/2013</td>
+                            <td>RM602</td>
+                            <td>James Doe</td>
+                            <td><button type="button" name="button" class="btn btn-primary">More...</button></td>
+                        </tr>
+                        <tr>
+                            <td>13</td>
+                            <td>PC001</td>
+                            <td>Computer</td>
+                            <td>20/02/2013</td>
+                            <td>RM602</td>
+                            <td>James Doe</td>
+                            <td><button type="button" name="button" class="btn btn-primary">More...</button></td>
+                        </tr>
+                        <tr>
+                            <td>14</td>
+                            <td>PC001</td>
+                            <td>Computer</td>
+                            <td>20/02/2013</td>
+                            <td>RM602</td>
+                            <td>James Doe</td>
+                            <td><button type="button" name="button" class="btn btn-primary">More...</button></td>
+                        </tr>
+                    </table>
+                 </article>
+              </section>
+          </div>
+      </div>
+    <!-- jQuery first, then Tether, then Bootstrap JS. -->
+    <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+    <script src="/js/bootstrap.min.js"></script>
+  </body>
+</html>

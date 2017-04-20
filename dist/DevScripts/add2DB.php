a521e02b00179c3f66b725b9331e83f55efbeeb3
Matthew Wood (0woodm62) <0woodm62@solent.ac.uk>
2017-04-20 12:46:30 +0100

    Add to DB modified

--- dist/DevScripts/add2DB.php
+++ dist/DevScripts/add2DB.php
@@ -1,16 +1,13 @@
 <?php
-require_once '../../vendor/autoload.php';
-require '../php/functions.php';
-$db = connect();
-//Populate the Database with Dubby Data for 50 Entries
-try {
-    for ($i=0; $i < 50; $i++) {
-        PopulateAssets($db);
+    require_once '../../vendor/autoload.php';
+    require '../php/functions.php';
+    $db = connect();
+    //Populate the Database with Dubby Data for 50 Entries
+    try {
+        for ($i=0; $i < 50; $i++) {
+            PopulateAssets($db);
+        }
+    } catch (Exception $e) {
+        echo "There Was An Error: </br>" . $e->getMessage();
     }
-} catch (Exception $e) {
-    echo "There Was An Error: </br>" . $e->getMessage();
-}
-
-
-
- ?>
+?>

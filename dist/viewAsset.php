1a13bf0ae59b3f807e4ec92afd6514944998a367
Matthew Wood (0woodm62) <0woodm62@solent.ac.uk>
2017-04-21 01:07:55 +0100

    view/Modify Assets Page Created.

---
+++ dist/viewAsset.php
@@ -0,0 +1,56 @@
+<?php
+require '/php/functions.php';
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>View Assets Details</title>
+        <link rel="stylesheet" href="/css/bootstrap.min.css">
+      </head>
+    <body>
+        <?php navigation(); ?>
+        <form class="" action="/php/modify.php" method="post">
+            <div class="form-group">
+                <label for="Assetno">Asset Number</label>
+                <input type="text" name="Assetno" class="form-control" placeholder="Asset Number" required  pattern="^[0-9]*$"/>
+            </div>
+            <div class="form-group">
+                <label for="assettype">Asset type</label>
+                <select class="form-control" name="assettype">
+                    <option value="Desktop">Desktop</option>
+                    <option value="Laptop">Laptop</option>
+                    <option value="HDD">Hard Drive</option>
+                </select>
+            </div>
+            <div class="form-group">
+                <label for="Make">Make</label>
+                <input type="text" name="Make" value="" class="form-control" required />
+            </div>
+            <div class="form-group">
+                <label for="Model">model</label>
+                <input type="text" name="Model" value="" class="form-control" required />
+            </div>
+            <div class="form-group">
+                <label for="Location">Location</label>
+                <input type="text" name="Location" class="form-control" required />
+            </div>
+            <div class="form-group">
+                <label for="PatTest">PatTested Date</label>
+                <input type="date" name="PatTest" class="form-control" required />
+            </div>
+            <div class="form-group">
+                <label for="User">associated To</label>
+                <input type="text" name="User" class="form-control" required />
+            </div>
+            <div class="form-group">
+                <input type="submit" class="form-control btn-primary" value="Update" />
+            </div>
+        </form>
+        <?php footer(); ?>
+        <script type="text/javascript">
+            var id = getQueryVariable("id");
+            getDetails(id);
+        </script>
+    </body>
+</html>
